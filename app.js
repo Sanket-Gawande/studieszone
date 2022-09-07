ff79b6cda3f1bbe841d6423958a9e520046aa1af
sanketg <sanketgawande.gcoey@gmail.com>
2022-09-08 04:58:42 +0530

    added client origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://studieszone-edtech.vercel.app"],
   })
 );
 
